31e4f881e5bd52a080e21bf71c4aa51c381b9584
thiagogagliari <thiagogagliari@hotmail.com>
2022-08-25 16:42:09 -0300

    Resolução exercícios Funções Array

---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3,4,5,6,7,8]
+
+const triplos = numbers.map((item)=>{return item*3})
+console.log(triplos);
+
+const metades = numbers.map((item)=>{return item/2})
+console.log(metades);
+
